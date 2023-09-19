32bb18d17a3f1fb77fa835e7935b9bbbf437938b
Camille Gautier <camille.gaut@sfr.fr>
2023-09-19 14:35:07 +0200

    [FIX] [CGA] fix warning on useEffect

--- src/app/provider/userProvider.js
+++ src/app/provider/userProvider.js
@@ -17,6 +17,7 @@ export default function UserProvider({ children }) {
   
    useEffect(() => {
       if (!user.isConnected) {router.push("/login");}
+      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [user.isConnected]);
 
    return (
